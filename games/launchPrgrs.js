7680fbe8c5dfdecc641abd5f26956811470df90b
nf <coderly@yandex.ru>
2022-12-05 20:28:31 +0300

    Stage 8.1.3 brain-progression: completed logic & added to package.json

--- games/launchPrgrs.js
+++ games/launchPrgrs.js
@@ -4,25 +4,23 @@ export default () => {
   const questionList = [];
   for (let counter = 0; counter < 3; counter += 1) {
     const questionPair = [];
-    const firstInt = getRandomInteger(2, 100);
-    const secondInt = getRandomInteger(2, 100);
-    let starter = firstInt;
-    if (firstInt > secondInt) {
-      if (Math.round(firstInt / 2) > secondInt) {
-        starter = secondInt;
-      } else starter = Math.round(firstInt / 2);
-    } else if (Math.round(secondInt / 2) > firstInt) {
-      starter = firstInt;
-    } else starter = Math.round(secondInt / 2);
-    for (starter; starter > 0; starter -= 1) {
-      if ((firstInt % starter === 0) && (secondInt % starter === 0)) {
-        break;
+    const firstInt = getRandomInteger(2, 80);
+    const step = getRandomInteger(2, 29);
+    const hiddenNumber = getRandomInteger(0, 9);
+    let question = '';
+    for (let counter2 = 0; counter2 < 10; counter2 += 1) {
+      if (counter2 === hiddenNumber) {
+        question = `${question} ..`;
+        questionPair[1] = String(firstInt + (step * counter2));
+      } else {
+        question = `${question} ${String(firstInt + (step * counter2))}`;
       }
     }
-    questionPair[0] = `${firstInt} ${secondInt}`;
-    questionPair[1] = String(starter);
+    questionPair[0] = question.trim();
+    question = '';
+    console.log(questionPair);
     questionList[counter] = questionPair;
   }
-  console.log('Find the greatest common divisor of given numbers.');
+  console.log('What number is missing in the progression?');
   return questionList;
 };
