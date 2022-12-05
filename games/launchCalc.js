ef4a16d1c2dd19cefa6b42bc7d1c0b067a57e88c
Kirill L <levitskiy.kirill@hexly.ru>
2022-12-05 13:00:44 +0300

    Stage 6.1.2 brain-calc logic completed

---
+++ games/launchCalc.js
@@ -0,0 +1,33 @@
+import { getRandomInteger } from '../src/numericFunc.js';
+
+export default () => {
+  const questionList = [];
+  for (let counter = 0; counter < 3; counter += 1) {
+    const questionPair = [];
+    const mathSign = getRandomInteger(1, 3);
+    const firstInt = getRandomInteger(3, 70);
+    let secondInt;
+    switch (mathSign) {
+      case 1:
+        secondInt = getRandomInteger(1, 29);
+        questionPair[0] = `${firstInt} + ${secondInt}`;
+        questionPair[1] = String(firstInt + secondInt);
+        break;
+      case 2:
+        secondInt = getRandomInteger(1, firstInt);
+        questionPair[0] = `${firstInt} - ${secondInt}`;
+        questionPair[1] = String(firstInt - secondInt);
+        break;
+      case 3:
+        secondInt = getRandomInteger(1, 10);
+        questionPair[0] = `${firstInt} * ${secondInt}`;
+        questionPair[1] = String(firstInt * secondInt);
+        break;
+      default:
+        break;
+    }
+    questionList[counter] = questionPair;
+  }
+  console.log('What is the result of the expression?');
+  return questionList;
+};
