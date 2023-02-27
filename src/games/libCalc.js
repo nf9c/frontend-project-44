4d83f5e4b5a0ab2a3fcd92b3a06cafa8d53f84be
Kirill L <levitskiy.kirill@hexly.ru>
2023-02-27 12:34:10 +0300

    Stage 10.4.3: libCalc.js reengineered

--- src/games/libCalc.js
+++ src/games/libCalc.js
@@ -3,29 +3,29 @@ import getRandomInteger from '../getRandomInteger.js';
 export default () => {
   const questionList = [];
   for (let counter = 0; counter < 3; counter += 1) {
+    const mathOperations = ['+', '-', '*'];
     const questionPair = [];
-    const mathSign = getRandomInteger(1, 3);
+    const mathSign = getRandomInteger(0, 2);
     const firstInt = getRandomInteger(3, 70);
-    let secondInt;
+    const secondInt = getRandomInteger(1, 10);
+    questionPair[0] = `${firstInt} ${mathOperations[mathSign]} ${secondInt}`;
+    let calculated = 0;
+
     switch (mathSign) {
+      case 0:
+        calculated = firstInt + secondInt;
+        break;
       case 1:
-        secondInt = getRandomInteger(1, 29);
-        questionPair[0] = `${firstInt} + ${secondInt}`;
-        questionPair[1] = String(firstInt + secondInt);
+        calculated = firstInt - secondInt;
         break;
       case 2:
-        secondInt = getRandomInteger(1, firstInt);
-        questionPair[0] = `${firstInt} - ${secondInt}`;
-        questionPair[1] = String(firstInt - secondInt);
-        break;
-      case 3:
-        secondInt = getRandomInteger(1, 10);
-        questionPair[0] = `${firstInt} * ${secondInt}`;
-        questionPair[1] = String(firstInt * secondInt);
+        calculated = firstInt * secondInt;
         break;
       default:
         break;
     }
+
+    questionPair[1] = String(calculated);
     questionList[counter] = questionPair;
   }
   console.log('What is the result of the expression?');
